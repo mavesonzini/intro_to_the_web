31ddfcdfabddc3ebc5cc5d2d9e72aa60085ae074
Veronica Sonzini <mavesonzini@gmail.com>
2016-11-23 09:53:59 +0000

    New commmit, working

--- MAKERS/web/app.rb
+++ MAKERS/web/app.rb
@@ -4,6 +4,9 @@ require 'sinatra'
 #   @name = ["Amigo", "Oscar", "Viking", "Veronica"].sample
 #   erb :index
 # end
+get '/cat-form'do
+  erb :cat_form
+end
 
 get '/named-cat' do
   @name = params[:name]
